10946beff67d9103216518d13cb96238709b8306
Martín Fenocchio <68252867+Novak-Fenocchio@users.noreply.github.com>
2023-11-19 12:06:51 -0300

    1.0.0

---
+++ index.js
@@ -0,0 +1,10 @@
+function copyText(texto) {
+  const textField = document.createElement("textarea");
+  textField.innerText = text;
+  document.body.appendChild(textField);
+  textField.select();
+  document.execCommand("copy");
+  textField.remove();
+}
+
+module.exports = copyText;
